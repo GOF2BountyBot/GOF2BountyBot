0e75c84e25a14bbd8ab8a0b42315dc8ed4e41162
Trimatix <1jasperlaw@gmail.com>
2021-08-30 18:59:29 +0100

    add generics to func type hint

--- bot/commandsManager/commandRegistry.py
+++ bot/commandsManager/commandRegistry.py
@@ -1,4 +1,4 @@
-from typing import Callable, List
+from typing import Callable, List, Awaitable
 from discord import Message
 from ..lib.exceptions import IncorrectCommandCallContext
 
@@ -9,7 +9,7 @@ class CommandRegistry:
     :var ident: The string command name by which this command is identified and called
     :vartype ident: str
     :var func: A reference to the function to call upon calling this CommandRegistry
-    :vartype func: Callable
+    :vartype func: Callable[[Message, str, bool], Awaitable[None]]
     :var forceKeepArgsCasing: Whether to pass arguments to the function with their original casing.
                                 If False, arguments will be transformed to lower case before passing.
     :vartype forceKeepArgsCasing: bool
@@ -25,12 +25,13 @@ class CommandRegistry:
     :vartype longHelp: str
     """
 
-    def __init__(self, ident: str, func: Callable, forceKeepArgsCasing: bool, forceKeepCommandCasing: bool,
-                    allowDM: bool, allowHelp: bool, aliases: List[str] = None, signatureStr: str = "", shortHelp: str = "",
-                    longHelp: str = "", helpSection: str = "miscellaneous"):
+    def __init__(self, ident: str, func: Callable[[Message, str, bool], Awaitable[None]], forceKeepArgsCasing: bool,
+                    forceKeepCommandCasing: bool, allowDM: bool, allowHelp: bool, aliases: List[str] = None,
+                    signatureStr: str = "", shortHelp: str = "", longHelp: str = "", helpSection: str = "miscellaneous"):
         """
         :param str ident: The string command name by which this command is identified and called
-        :param Callable func: A reference to the function to call upon calling this CommandRegistry
+        :param func: A reference to the coroutine to call upon calling this CommandRegistry
+        :type func: Callable[[Message, str, bool], Awaitable[None]]
         :param bool forceKeepArgsCasing: Whether to pass arguments to the function with their original casing.
                                         If False, arguments will be transformed to lower case before passing.
         :param bool forceKeepCommandCasing: Whether the command must be called with exactly the correct casing
