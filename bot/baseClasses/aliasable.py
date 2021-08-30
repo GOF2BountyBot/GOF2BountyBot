e51a667f6f380a8a4ef6e9900cadf5559583628d
Trimatix <1jasperlaw@gmail.com>
2021-08-30 17:21:40 +0100

    accept 'object' for Aliasable.__eq__ '

--- bot/baseClasses/aliasable.py
+++ bot/baseClasses/aliasable.py
@@ -1,6 +1,6 @@
 # Typing imports
 from __future__ import annotations
-from typing import List
+from typing import List, cast
 from . import serializable
 
 from abc import abstractmethod
@@ -39,14 +39,17 @@ class Aliasable(serializable.Serializable):
             self.aliases += [name.lower()]
 
 
-    def __eq__(self, other : Aliasable) -> bool:
+    def __eq__(self, other : object) -> bool:
         """Decide Aliasable equality based on either object sharing the other's main name.
         If neither object has the other's main name as an alias,
         False will be returned regardless of whether any other aliases are shared.
 
         :param Aliasable other: Object to check for equivilance
         """
-        return type(other) == type(self) and self.isCalled(other.name) or other.isCalled(self.name)
+        if type(other) == type(self):
+            other = cast(Aliasable, other)
+            return self.isCalled(other.name) or other.isCalled(self.name)
+        return False
 
 
     def isCalled(self, name : str) -> bool:
