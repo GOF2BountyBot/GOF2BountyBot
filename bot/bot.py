ff6c246967d26dd6c80f6bc730823dd2423b77de
Trimatix <1jasperlaw@gmail.com>
2021-10-09 19:07:12 +0100

    call the EmojisConfig initializer instead of initializing emojis manually

--- bot/bot.py
+++ bot/bot.py
@@ -51,50 +51,6 @@ async def checkForUpdates():
                     + versionInfo.BASED_REPO_URL + " for instructions on how to update your BASED fork.")
 
 
-async def initializeEmojis():
-    """Converts all of the expected emoji config vars from UninitializedBasedEmoji to BasedEmoji.
-    Throws errors if initialization of any emoji failed.
-    """
-    emojiVars = []
-    emojiListVars = []
-
-    # Gather attribute names of emoji config vars
-    for varname in cfg.defaultEmojis.attrNames:
-        varvalue = getattr(cfg.defaultEmojis, varname)
-
-        # ensure single emoji vars are emojis
-        if type(varvalue) == lib.emojis.UninitializedBasedEmoji:
-            emojiVars.append(varname)
-            continue
-
-        # ensure list emoji vars only contain emojis
-        elif type(varvalue) == list:
-            onlyEmojis = True
-            for item in varvalue:
-                if type(item) != lib.emojis.UninitializedBasedEmoji:
-                    onlyEmojis = False
-                    break
-            if onlyEmojis:
-                emojiListVars.append(varname)
-                continue
-
-        # raise an error on unexpected types
-        raise ValueError("Invalid config variable in cfg.defaultEmojis: Emoji config variables must be either " \
-                            + "UninitializedBasedEmoji or List[UninitializedBasedEmoji]")
-
-    # Initialize emoji vars
-    for varname in emojiVars:
-        setattr(cfg.defaultEmojis, varname, lib.emojis.BasedEmoji.fromUninitialized(getattr(cfg.defaultEmojis, varname)))
-
-    # Initialize lists of emojis vars
-    for varname in emojiListVars:
-        working = []
-        for item in getattr(cfg.defaultEmojis, varname):
-            working.append(lib.emojis.BasedEmoji.fromUninitialized(item))
-
-        setattr(cfg.defaultEmojis, varname, working)
-
-
 def setHelpEmbedThumbnails():
     """Loads the bot application's profile picture into all help menu embeds as the embed thumbnail.
     If no profile picture is set for the application, the default profile picture is used instead.
@@ -460,12 +416,7 @@ async def on_ready():
     ##### EMOJI INITIALIZATION #####
 
     # Convert all UninitializedBasedEmojis in config to BasedEmoji
-    await initializeEmojis()
-
-    # Ensure all emojis have been initialized
-    for varName, varValue in vars(cfg).items():
-        if isinstance(varValue, lib.emojis.UninitializedBasedEmoji):
-            raise RuntimeError("Uninitialized emoji still remains in cfg after emoji initialization: '" + varName + "'")
+    cfg.defaultEmojis.initAll()
 
 
     ##### GAME OBJECTS LOADING #####
