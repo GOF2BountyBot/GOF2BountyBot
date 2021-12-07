a28a8eb1b80e2875f6fccf5a74fac7ce71546473
Trimatix <1jasperlaw@gmail.com>
2021-12-07 22:46:32 +0000

    fix missing comma

--- bot/cfg/cfg.py
+++ bot/cfg/cfg.py
@@ -41,7 +41,7 @@ defaultEmojis = cast(Dict[str, Union[BasedEmoji, List[BasedEmoji]]], {
     # Emoji sent with new bounty listings
     "newBounty": UninitializedBasedEmoji("⛓"),
 
-    "bountyRespawn": UninitializedBasedEmoji("⛓")
+    "bountyRespawn": UninitializedBasedEmoji("⛓"),
 
     "newIssue": UninitializedBasedEmoji("📥"),
     "issueClosed": UninitializedBasedEmoji("✅"),
