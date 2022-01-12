333142e7a570ab2d8dcc7d033c2a7ed82685828c
Trimatix <1jasperlaw@gmail.com>
2022-01-12 22:18:59 +0000

    fix typo

--- bot/commands/util_autoskin.py
+++ bot/commands/util_autoskin.py
@@ -189,7 +189,7 @@ async def collectAutoskinArgs(message: discord.Message, userShipName: str, res_x
         if baseTex.mode == "RGBA":
             ext = "png"
         else:
-            baseText = baseTex.convert("RGB")
+            baseTex = baseTex.convert("RGB")
             ext = "jpg"
         skinPaths[key] = os.path.join(CWD, cfg.paths.rendererTempFolder, f"{message.id}_{key}.{ext}")
         baseTex.save(skinPaths[key])
