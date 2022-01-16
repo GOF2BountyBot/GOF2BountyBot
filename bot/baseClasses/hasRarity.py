186e5765658f628d1191cabc0512aa28cc6cc764
Trimatix <1JasperLaw@gmail.com>
2022-01-16 18:02:56 +0000

    add HasRarity mixin

---
+++ bot/baseClasses/hasRarity.py
@@ -0,0 +1,9 @@
+class HasRarity:
+    """A mixin that simply ensures the existence of the `rarityLevel` attribute.
+    """
+
+    def __init__(self, rarityLevel: int, *args, **kwargs):
+        self.rarityLevel = rarityLevel
+        super().__init__(*args, **kwargs)
+
+    
