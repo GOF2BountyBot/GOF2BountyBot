51f33d275e7a68a3519be1659fa92b0a90b4c0c0
Trimatix <1jasperlaw@gmail.com>
2022-01-16 22:31:27 +0000

    select item rarities from cumulative distribution

--- bot/lib/gameMaths.py
+++ bot/lib/gameMaths.py
@@ -258,6 +258,9 @@ def pickRandomCriminalTL() -> int:
     return cfg.maxTechLevel
 
 
+itemRaritiesDistributionReverseCumulative = makeCumulative(list(cfg.itemRaritiesDistribution[::-1]))
+
+
 def pickRandomItemRarityLevel() -> int:
     """Pick a random item rarity level, according to the weightings in cfg.itemRaritiesDistribution.
 
@@ -271,7 +274,7 @@ def pickRandomItemRarityLevel() -> int:
         # Subtracting from minRarity because I'm iterating over the reverse of the rarities distribution,
         # so indices will be reversed as well.
         return next(minRarity - level \
-                    for level, weight in enumerate(cfg.itemRaritiesDistribution[::-1]) \
+                    for level, weight in enumerate(itemRaritiesDistributionReverseCumulative) \
                     if weight >= rarityChance)
     except StopIteration:
         return 0
