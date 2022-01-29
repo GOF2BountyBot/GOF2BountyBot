bf7f3397af6ae633d7ec6a08996a91e14edd04b3
Trimatix <1JasperLaw@gmail.com>
2022-01-29 14:26:38 +0000

    disable homeguild check on cmd_pay

--- bot/commands/usr_economy.py
+++ bot/commands/usr_economy.py
@@ -539,31 +539,11 @@ async def cmd_pay(message : discord.Message, args : str, isDM : bool):
     else:
         targetBBUser: basedUser.BasedUser = botState.usersDB.addID(requestedUser.id)
 
-    homeGuild: discord.Guild = botState.client.get_guild(sourceBBUser.homeGuildID)
-
-    if not targetBBUser.hasHomeGuild() or not sourceBBUser.hasHomeGuild() or \
-            targetBBUser.homeGuildID != sourceBBUser.homeGuildID:
-        await message.channel.send(":x: You can only pay players whose home server is " \
-                                    + homeGuild.name + "!")
-        return
-
     sourceBBUser.credits -= amount
     targetBBUser.credits += amount
 
     await message.reply(mention_author=False, content=":moneybag: You paid " + lib.discordUtil.userOrMemberName(requestedUser, message.guild) \
                                 + " **" + str(amount) + "** credits!")
-    
-    if message.guild.get_member(requestedUser.id) is None:
-        homeBGuild: basedGuild.BasedGuild = botState.guildsDB.getGuild(homeGuild.id)
-        if homeBGuild.hasPlayChannel():
-            await homeBGuild.getPlayChannel().send(f":moneybag: {message.author.mention} paid " \
-                                                    + f"{requestedUser.mention} **{amount}** credits!")
-        else:
-            try:
-                await requestedUser.send(f":moneybag: {message.author.mention} paid you **{amount}** credits!")
-            except (discord.Forbidden, discord.HTTPException, discord.NotFound) as e:
-                botState.logger.log("user_economy", "cmd_pay", "Exception thrown when attempting to DM pay announcement",
-                                    exception=e)
         
 
 botCommands.register("pay", cmd_pay, 0, forceKeepArgsCasing=True, allowDM=True, helpSection="economy",
